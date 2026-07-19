baa0dadf967b230f8b8ed2670e91bf735c679abb
AMD-2009Yes <antonmilogolovkin@yandex.ru>
2026-07-19 21:25:51 +0400

    Delete assets/js/scrolling.js

--- assets/js/scrolling.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("scroll", () => {
-  document.querySelectorAll(".animate-on-scroll").forEach(el => {
-    const pos = el.getBoundingClientRect().top;
-    if (pos < window.innerHeight - 100) {
-      el.classList.add("animate-fade-in");
-    }
-  });
-});
